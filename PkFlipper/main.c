87e9851638be284d60edabdccf4b1cee225cab86
Alex Lelievre <alex@folabs.com>
2021-05-25 11:21:11 -0700

    first commit

---
+++ PkFlipper/main.c
@@ -0,0 +1,91 @@
+//
+//  main.c
+//  PkFlipper
+//
+//  Created by Alex Lelievre on 5/24/21.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <mach/mach_time.h>
+#include <unistd.h>
+
+#define num_bits        200
+#define count_per_sec   20
+#define count_period    60
+#define sleep_period_us (1000000 / count_per_sec)
+#define num_obs         (count_per_sec * count_period)
+
+#define num_bits_as_num_longs (num_bits / 32)
+
+
+
+int count_bits( uint32_t value )
+{
+    int bit_count = 0;
+    for( int i = 0; i < 32; i++ )
+    {
+        if( value & (1 << i) )
+            ++bit_count;
+    }
+    return bit_count;
+}
+
+
+uint32_t myrandom( void )
+{
+    return arc4random();
+//    return 0xFFFFFFFF;      // for testing to prove I'm getting 200 bits when all bits are lit
+}
+
+
+
+int main( int argc, const char * argv[] )
+{
+    printf("PkFlipper\n");
+    uint32_t seconds = 0;
+
+    float chi_squared     = 0;
+    int   bit_count       = 0;
+    bool  intent_higher   = true;
+    
+    while( 1 )
+    {
+        // do this loop 20 times and wait 50ms inbetween gives us a full second of readings...
+        for( int x = 0; x < count_per_sec; x++ )
+        {
+            bit_count = 0;
+            for( int i = 0; i < num_bits_as_num_longs; i++ )
+            {
+                uint32_t rando = myrandom();
+                bit_count += count_bits( rando );
+            }
+
+            // also add remaining 8 bits of data (200/32 has a remainder of 8)
+            bit_count += count_bits( myrandom() & 0xFF );
+            chi_squared += powf(bit_count - 100, 2) / 100;
+            usleep( sleep_period_us );
+        }
+
+        ++seconds;
+
+        printf( "intent: %s, chi_squared: %3.0f, seconds remaining = %d\n", intent_higher ? "higher" : "lower", chi_squared, 60 - seconds );
+//        printf( "bit_count: %d, chi_squared: %4.4f, seconds remaining = %d\n", bit_count, chi_squared, 60 - seconds );
+
+        if( seconds >= count_period )
+        {
+            for( int i = 1; i <= 60; i++ )
+            {
+                sleep( 1 );
+                printf( "last score: %3.0f, last intent: %s, next session in %d secs\n", chi_squared, intent_higher ? "higher" : "lower", 60 - i );
+            }
+
+            seconds = 0;
+            chi_squared = 0;
+            intent_higher = !intent_higher;
+        }
+    }
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+PkFlipper: Flipping Bits with PsychoKinesis
+
+Based on the the Global Consciousness Project, PkFlipper is a game of using your mind to influence the outcome of a Hardware Random Number Generator. This RNG uses the avalanche effect in a semiconductor junction to generate true random numbers, unlike a typical computer based deterministic random bit generator (pseudorandom number generator). According to studies, these types of HRNG devices can be influenced by human consciousness and intent.
+
+Bringing People Together Through High Scores
+
+A one minute game happens every 2 minutes. 200 bits are counted 20 times a second during this minute. That works out to 100 bits on average over the minute. The Chi-Square Test is used to accumulate the amount of variation from the theoretical variation of 0.  An average accumulation without conscious intervention is somewhere around 600 over the one minute period.  Anything above or below that value is significant. The more people focusing the on the same intent, skewing the RNG average, the more likely it is to happen. Ideally, this is a multiplayer game and location should not matter. However, there is no reason one person can't skew the average.
